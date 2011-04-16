8b4e5d98c004d7df235ffc63417d9f29929f2297
velna <velna_007@163.com>
2011-04-16 14:28:53 +0800

    update
    Signed-off-by: velna <velna_007@163.com>

---
+++ src/main/java/org/sothis/web/mvc/ModelAndViewSupport.java
@@ -0,0 +1,41 @@
+package org.sothis.web.mvc;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ModelAndViewSupport implements ModelAndView {
+
+	private String viewType = ModelAndViewResolver.DEFAULT_VIEW_KEY;
+	private Map<String, Object> viewParams = new HashMap<String, Object>();
+
+	@Override
+	public Object getModel() {
+		return this;
+	}
+
+	@Override
+	public String getViewType() {
+		return viewType;
+	}
+
+	@Override
+	public Map<String, Object> getViewParams() {
+		return viewParams;
+	}
+
+	public void setViewType(String viewType) {
+		this.viewType = viewType;
+	}
+
+	public void setViewParams(Map<String, Object> viewParams) {
+		this.viewParams = viewParams;
+	}
+
+	public void setViewParam(String paramName, Object paramValue) {
+		viewParams.put(paramName, paramValue);
+	}
+
+	public Object getViewParam(String paramName) {
+		return viewParams.get(paramName);
+	}
+}
