838159f856d034acd4bf74c5e71eb1f0cdd53a26
velna <velna_007@163.com>
2012-08-22 11:53:42 +0800

    add file upload example

---
+++ sothis-example/src/main/java/org/sothis/web/example/controllers/FileUploadController.java
@@ -0,0 +1,16 @@
+package org.sothis.web.example.controllers;
+
+import org.sothis.web.example.models.BaseModel;
+import org.sothis.web.example.models.UploadModel;
+
+public class FileUploadController {
+
+	public Object indexAction(BaseModel model) {
+		return model;
+	}
+
+	public Object uploadAction(UploadModel model) {
+		return model;
+	}
+
+}
---
+++ sothis-example/src/main/webapp/fileUpload/upload.ftl
@@ -0,0 +1,4 @@
+image path is: ${image.absolutePath}<br />
+image client file name is: ${imageFileName}<br />
+image content typs is: ${imageContentType}<br />
+image input stream: ${imageInputStream}<br />
